7a281bbcf5cdcc52eec841daf39e23abd83e09fd
jukno0416 <77935558+jukno0416@users.noreply.github.com>
2021-03-25 00:22:00 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div>
+         
+            <div class="footer">
+                    <div class="clearfix"> </div>
+                        <p style="font-family: cursive;">Whisk Dance Studio System @ 2021  </p>
+            </div>
+        </div>
